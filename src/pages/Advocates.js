4a020393dd159a579e0d8c3cb9422133fc72b7cd
macourage18 <macourage17@gmail.com>
2023-12-28 13:36:59 +0100

    first commit

---
+++ src/pages/Advocates.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Section1 } from '../components/advocates/Section1'
+
+export const Advocates = () => {
+  return (
+    <div>
+      <Section1 />
+    </div>
+  )
+}
+
+ 
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import { Section1 } from "../components/home/Section1"
+
+export const Home = ()=>{
+  return(
+    <div>
+      <Section1 />
+    </div>
+  )
+}
